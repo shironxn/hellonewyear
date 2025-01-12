67846a2288a020f7622577228db9841f15958fe5
shironxn <shironxn@gmail.com>
2025-01-12 08:27:40 +0700

    chore: update font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,11 +18,7 @@ export default async function Home() {
       <div className="flex justify-center flex-col gap-8 w-full">
         <div className="flex flex-col items-center justify-center gap-2 text-4xl md:text-6xl font-medium mb-4 text-center">
           <span>
-            New Year
-          </span>
-          <br />
-          <span>
-            Countdown {new Date().getFullYear() + 1}
+            New Year Countdown {new Date().getFullYear() + 1}
           </span>
         </div>
         <Countdown />
