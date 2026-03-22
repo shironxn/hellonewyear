348ab0fd2209f70dc484ddfa83c02c15f1b981cd
shironxn <shironxn@gmail.com>
2026-03-22 17:39:51 +0700

    chore: increase main content top padding and refine footer heart icon display.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,8 +7,8 @@ export function Footer() {
     <footer className="footer footer-center bg-base-300 md:text-lg p-4 text-center">
       <aside>
         <p className="flex items-center justify-center gap-1">
-          Copyright © {new Date().getFullYear()} - All right reserved by
-          shiron, built with <Heart className="h-4 w-4 inline" />
+          Copyright © {new Date().getFullYear()} - All right reserved by shiron,
+          built with <Heart className="h-4 w-4" />
         </p>
       </aside>
     </footer>
